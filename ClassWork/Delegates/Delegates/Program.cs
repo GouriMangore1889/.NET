6567d6fa61fb6c0fd88166c99705e34748a238b6
Gouri Mangore <mangoregouri@gmail.com>
2026-02-01 00:37:22 +0530

    Delegates

---
+++ ClassWork/Delegates/Delegates/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public delegate void mydel();
+    public static void f1()
+    {
+        Console.WriteLine("f1");
+
+    }
+     public static void f2()
+    {
+        Console.WriteLine("f2");
+    }
+    
+    
+    public static void Main(string[] args)
+    {
+        mydel del = f1;
+        del+=f2;
+        del();
+    }
+}
