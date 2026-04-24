ea3d304ede382da3ef4191be7c67d0d2cdc58c94
Gouri Mangore <mangoregouri@gmail.com>
2026-04-24 23:48:52 +0530

    MVC

---
+++ LabWork/Exp06_MVC/crmAdvanc/Models/Customer.cs
@@ -0,0 +1,13 @@
+
+public class Customer
+{
+    public int ID{get ;set ;}
+    public string name { get; set; }
+    public string email { get; set; }
+     public int phone { get; set; }
+
+     
+
+
+
+}
